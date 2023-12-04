cc285827d3dee84f99dcefaf195a2a651dba5ef7
DevExpressExampleBot <fakeEmail>
2023-12-04 20:39:16 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,28 +1,27 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Windows.Forms
 Imports System.Data.OleDb
 Imports System.Data
+Imports RichEditCustomInsertMergeFieldMenu.Properties
 
 Namespace RichEditCustomInsertMergeFieldMenu
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class Form1
+        Inherits Form
 
-			richEditControl1.Options.MailMerge.DataSource = GetMailMergeData()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            richEditControl1.Options.MailMerge.DataSource = GetMailMergeData()
+        End Sub
 
-		Private Function GetMailMergeData() As Object
-			Dim connection As New OleDbConnection(My.Settings.Default.nwindConnectionString)
-			Dim commandText As String = "SELECT Employees.*, Customers.* FROM (Employees INNER JOIN EmployeeCustomers ON Employees.EmployeeID = EmployeeCustomers.EmployeeId) INNER JOIN Customers ON EmployeeCustomers.CustomerId = Customers.CustomerID;"
-			Dim selectCommand As OleDbCommand = New System.Data.OleDb.OleDbCommand(commandText, connection)
-			Dim dataAdapter As New OleDbDataAdapter(selectCommand)
-			Dim dataSet As New DataSet()
-			dataAdapter.Fill(dataSet)
-			Return dataSet.Tables(0)
-		End Function
-	End Class
+        Private Function GetMailMergeData() As Object
+            Dim connection As OleDbConnection = New OleDbConnection(Settings.Default.nwindConnectionString)
+            Dim commandText As String = "SELECT Employees.*, Customers.* FROM (Employees INNER JOIN EmployeeCustomers ON Employees.EmployeeID = EmployeeCustomers.EmployeeId) INNER JOIN Customers ON EmployeeCustomers.CustomerId = Customers.CustomerID;"
+            Dim selectCommand As OleDbCommand = New OleDbCommand(commandText, connection)
+            Dim dataAdapter As OleDbDataAdapter = New OleDbDataAdapter(selectCommand)
+            Dim dataSet As DataSet = New DataSet()
+            dataAdapter.Fill(dataSet)
+            Return dataSet.Tables(0)
+        End Function
+    End Class
 End Namespace
